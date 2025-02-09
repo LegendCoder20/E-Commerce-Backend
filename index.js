4486e40b70d23010f9a449e50f1a0d35cbd722eb
LegendCoder20 <20sit074.manjarekar.a20@gmail.com>
2025-02-09 21:54:35 +0530

    Added CORS

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
 app.use(morgan("combined"));
